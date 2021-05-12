bf24970591c6016bd2656f42f40fde39773905d5
Rio Arjuna <ri0arjuna@programmer.net>
2021-05-12 20:17:16 +0700

    add function to open list of doa

--- app/src/main/java/skripsi/cika/doaharianpaud/MainActivity.kt
+++ app/src/main/java/skripsi/cika/doaharianpaud/MainActivity.kt
@@ -1,11 +1,25 @@
 package skripsi.cika.doaharianpaud
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import skripsi.cika.doaharianpaud.databinding.ActivityMainBinding
+import skripsi.cika.doaharianpaud.listdoa.ListDoaActivity
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    binding.buttonDoa.setOnClickListener {
+      val intent = Intent(this, ListDoaActivity::class.java)
+      startActivity(intent)
+    }
+
   }
 }
