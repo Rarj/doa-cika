eea08f2619d24288f35266f463c538c1d66d43c5
Rio Arjuna <ri0arjuna@programmer.net>
2021-05-12 15:06:45 +0700

    create dummy doa in DoaRepository.kt

---
+++ app/src/main/java/skripsi/cika/doaharianpaud/repo/listdoa/DoaModel.kt
@@ -0,0 +1,8 @@
+package skripsi.cika.doaharianpaud.repo.listdoa
+
+data class DoaModel(
+  val id: String,
+  val doaName: String,
+  val arabic: String,
+  val translate: String
+)
---
+++ app/src/main/java/skripsi/cika/doaharianpaud/repo/listdoa/DoaRepository.kt
@@ -0,0 +1,27 @@
+package skripsi.cika.doaharianpaud.repo.listdoa
+
+class DoaRepository {
+
+  fun getListDoa(): List<DoaModel> {
+    return listOf(
+      DoaModel(id = "1", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "2", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "3", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "4", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "5", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "6", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "7", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "8", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "9", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "10", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "11", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "12", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "13", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "14", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "15", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "16", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+      DoaModel(id = "17", doaName = "Doa", arabic = "الفاتحة", translate = "Translate"),
+    )
+  }
+
+}
