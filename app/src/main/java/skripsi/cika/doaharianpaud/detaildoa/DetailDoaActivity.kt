91b24af6aed160f0ee74ddc3e894367b33ef8606
Rio Arjuna <ri0arjuna@programmer.net>
2021-05-12 20:37:28 +0700

    add blank DetailDoaActivity.kt

---
+++ app/src/main/java/skripsi/cika/doaharianpaud/detaildoa/DetailDoaActivity.kt
@@ -0,0 +1,12 @@
+package skripsi.cika.doaharianpaud.detaildoa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import skripsi.cika.doaharianpaud.R
+
+class DetailDoaActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail_doa)
+  }
+}
