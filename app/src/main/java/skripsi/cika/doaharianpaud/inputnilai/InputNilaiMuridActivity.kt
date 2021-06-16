82c066def35885f10cda061047ef037e83931e90
Rio Arjuna <ri0arjuna@programmer.net>
2021-06-16 11:12:37 +0700

    add new InputNilaiMuridActivity.kt

---
+++ app/src/main/java/skripsi/cika/doaharianpaud/inputnilai/InputNilaiMuridActivity.kt
@@ -0,0 +1,12 @@
+package skripsi.cika.doaharianpaud.inputnilai
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import skripsi.cika.doaharianpaud.R
+
+class InputNilaiMuridActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_input_nilai_murid)
+  }
+}
